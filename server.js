14969b3d944c761edad85c08cbc3fea7c0338bdf
Ajay Konda <94820332+ajayk134@users.noreply.github.com>
2023-11-01 01:26:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://front-end-lw8k.onrender.com/',
+    origin: 'https://front-end-lw8k.onrender.com',
     methods: ['GET', 'POST']
   }
 })
